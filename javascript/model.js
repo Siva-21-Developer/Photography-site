74e8e9ccaefa58bc0049ab23e4d76bd9059404fe
siva <siva2122001@gmail.com>
2024-05-01 09:13:07 +0530

    first commit

---
+++ javascript/model.js
@@ -0,0 +1,31 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    document.getElementById("navbar").style.position = 'fixed';
+    } else {
+      document.getElementById("navbar").style.position = 'absolute';
+    }
+}
+
+// function myFunction() {
+//   window.setTimeout(function(){ window.location = "http://www.yoururl.com"; },1000);
+// }
+
+document.addEventListener('contextmenu', (e) => e.preventDefault());
+
+
+function ctrlShiftKey(e, keyCode) {
+  return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
+}
+
+document.onkeydown = (e) => {
+  if (
+    event.keyCode === 123 ||
+    ctrlShiftKey(e, 'I') ||
+    ctrlShiftKey(e, 'J') ||
+    ctrlShiftKey(e, 'C') ||
+    (e.ctrlKey && e.keyCode === 'U'.charCodeAt(0))
+  )
+    return false;
+};
